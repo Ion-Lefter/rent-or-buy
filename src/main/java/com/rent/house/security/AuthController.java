9a1793f9477958d8059f753df45b4eeb75ad9b6f
Ion Lefter <ionlefter1997@gmail.com>
2024-09-05 16:46:56 +0300

    user login

--- src/main/java/com/rent/house/security/AuthController.java
+++ src/main/java/com/rent/house/security/AuthController.java
@@ -3,6 +3,10 @@ package com.rent.house.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -18,12 +22,25 @@ public class AuthController {
     private AppUserRepository appUserRepository;
     private RoleRepository roleRepository;
     private PasswordEncoder passwordEncoder;
+
+    private AuthenticationManager authenticationManager;
+//    @Autowired
+//    public AuthController(AppUserRepository appUserRepository,
+//                          RoleRepository roleRepository, PasswordEncoder passwordEncoder) {
+//        this.appUserRepository = appUserRepository;
+//        this.roleRepository = roleRepository;
+//        this.passwordEncoder = passwordEncoder;
+//    }
+
+
+
     @Autowired
     public AuthController(AppUserRepository appUserRepository,
-                          RoleRepository roleRepository, PasswordEncoder passwordEncoder) {
+                          RoleRepository roleRepository, PasswordEncoder passwordEncoder, AuthenticationManager authenticationManager) {
         this.appUserRepository = appUserRepository;
         this.roleRepository = roleRepository;
         this.passwordEncoder = passwordEncoder;
+        this.authenticationManager = authenticationManager;
     }
 
     @PostMapping("register")
@@ -44,4 +61,16 @@ public class AuthController {
 
         return new ResponseEntity<>("User registered success!", HttpStatus.OK);
     }
+
+    @PostMapping("login")
+    public ResponseEntity<String> login(@RequestBody LoginDto loginDto) {
+
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(loginDto.getUsername(), loginDto.getPassword()));
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        String response = "You have successfully logged in!";
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+
+
 }
---
+++ src/main/java/com/rent/house/security/LoginDto.java
@@ -0,0 +1,9 @@
+package com.rent.house.security;
+
+import lombok.Data;
+
+@Data
+public class LoginDto {
+    private String username;
+    private String password;
+}
